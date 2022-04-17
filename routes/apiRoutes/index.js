ea5ccc8eadb8ba2d49fbe9d22ffca7f717c77805
emilyk <emily.g.kruse@gmail.com>
2022-04-16 19:44:35 -0500

    add unique id

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const { createNewNote } = require("../../lib/notes.js");
 const { notes } = require("../../db/db.json");
+const { v4: uuidv4 } = require('uuid');
 
 router.get("/notes", (req, res) => {
   let results = notes;
@@ -9,6 +10,7 @@ router.get("/notes", (req, res) => {
 
 router.post("/notes", (req, res) => {
   // add a unique id
+  req.body.id = uuidv4();
   const note = createNewNote(req.body, notes);
   res.json(note);
   console.log("post");
